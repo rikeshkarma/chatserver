c236142d5cbed151db2919dae82d58bf229500d3
Rikesh Karmacharya <rikeshkarmacharya.rk@gmail.com>
2021-09-26 12:18:46 +0545

    lint errors fixed

--- chat/tests.py
+++ chat/tests.py
@@ -24,53 +24,61 @@ class ChatTests(ChannelsLiveServerTestCase):
 
     def test_when_chat_message_posted_then_seen_by_everyone_in_same_room(self):
         try:
-            self._enter_chat_room('room_1')
+            self._enter_chat_room("room_1")
 
             self._open_new_window()
-            self._enter_chat_room('room_1')
+            self._enter_chat_room("room_1")
 
             self._switch_to_window(0)
-            self._post_message('hello')
-            WebDriverWait(self.driver, 2).until(lambda _:
-                                                'hello' in self._chat_log_value,
-                                                'Message was not received by window 1 from window 1')
+            self._post_message("hello")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self.chat_log_value,
+                "Message was not received by window 1 from window 1",
+            )
             self._switch_to_window(1)
-            WebDriverWait(self.driver, 2).until(lambda _:
-                                                'hello' in self._chat_log_value,
-                                                'Message was not received by window 2 from window 1')
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 2 from window 1",
+            )
         finally:
             self._close_all_new_windows()
 
     def test_when_chat_message_posted_then_not_seen_by_anyone_in_different_room(self):
         try:
-            self._enter_chat_room('room_1')
+            self._enter_chat_room("room_1")
 
             self._open_new_window()
-            self._enter_chat_room('room_2')
+            self._enter_chat_room("room_2")
 
             self._switch_to_window(0)
             self._post_message('hello')
-            WebDriverWait(self.driver, 2).until(lambda _:
-                                                'hello' in self._chat_log_value,
-                                                'Message was not received by window 1 from window 1')
+            self._post_message("hello")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "hello" in self._chat_log_value,
+                "Message was not received by window 1 from window 1",
+            )
 
             self._switch_to_window(1)
-            self._post_message('world')
-            WebDriverWait(self.driver, 2).until(lambda _:
-                                                'world' in self._chat_log_value,
-                                                'Message was not received by window 2 from window 2')
-            self.assertTrue('hello' not in self._chat_log_value,
-                            'Message was improperly received by window 2 from window 1')
+            self._post_message("world")
+            WebDriverWait(self.driver, 2).until(
+                lambda _: "world" in self._chat_log_value,
+                "Message was not received by window 2 from window 2",
+            )
+            self.assertTrue(
+                "hello" not in self._chat_log_value,
+                "Message was improperly received by window 2 from window 1",
+            )
         finally:
             self._close_all_new_windows()
 
     # === Utility ===
 
     def _enter_chat_room(self, room_name):
-        self.driver.get(self.live_server_url + '/chat/')
-        ActionChains(self.driver).send_keys(room_name + '\n').perform()
-        WebDriverWait(self.driver, 2).until(lambda _:
-                                            room_name in self.driver.current_url)
+        self.driver.get(self.live_server_url + "/chat/")
+        ActionChains(self.driver).send_keys(room_name + "").perform()
+        WebDriverWait(self.driver, 2).until(
+            lambda _: room_name in self.driver.current_url
+        )
 
     def _open_new_window(self):
         self.driver.execute_script('window.open("about:blank", "_blank");')
@@ -79,7 +87,7 @@ class ChatTests(ChannelsLiveServerTestCase):
     def _close_all_new_windows(self):
         while len(self.driver.window_handles) > 1:
             self.driver.switch_to_window(self.driver.window_handles[-1])
-            self.driver.execute_script('window.close();')
+            self.driver.execute_script("window.close();")
         if len(self.driver.window_handles) == 1:
             self.driver.switch_to_window(self.driver.window_handles[0])
 
@@ -87,8 +95,10 @@ class ChatTests(ChannelsLiveServerTestCase):
         self.driver.switch_to_window(self.driver.window_handles[window_index])
 
     def _post_message(self, message):
-        ActionChains(self.driver).send_keys(message + '\n').perform()
+        ActionChains(self.driver).send_keys(message + "").perform()
 
     @property
     def _chat_log_value(self):
-        return self.driver.find_element_by_css_selector('#chat-log').get_property('value')
+        return self.driver.find_element_by_css_selector("#chat-log").get_property(
+            "value"
+        )
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    path("chat/", include("chat.urls")),
+    path("admin/", admin.site.urls),
 ]
